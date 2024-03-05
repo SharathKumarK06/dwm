e2ce57f184b18078993cda8050895b879afc8a54
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-03-05 11:09:17 +0530

    Remove 'alternativetags' patch

--- config.h
+++ config.h
@@ -24,8 +24,7 @@ const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const int momentaryalttags = 1; /* 1 means alttags will show only when key is held down*/
+
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
@@ -108,7 +107,6 @@ static const Key keys[] = {
 	{ MODKEY,                               XK_comma,           focusmon,           {.i = -1 } },
 	{ MODKEY|ShiftMask,                     XK_period,          tagmon,             {.i = +1 } },
 	{ MODKEY|ShiftMask,                     XK_comma,           tagmon,             {.i = -1 } },
-	{ MODKEY|ShiftMask,                     XK_y,               togglealttag,       {0} },
 	{ MODKEY,                               XK_s,               togglescratch,      {.ui = 0 } },
 	{ MODKEY,                               XK_c,               movecenter,         {0} },
 	{ MODKEY,                               XK_n,               viewadjacent,       {.i = +1 } },
